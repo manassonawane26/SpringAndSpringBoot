63c100a727f926d4fe833a3d22a09bd7e43ef2fc
manassonawane26 <manassonawane26@gmail.com>
2024-10-09 09:34:18 +0530

    Further things with Spring Framework

--- SpringDemo/src/main/java/org/spring/Alien.java
+++ SpringDemo/src/main/java/org/spring/Alien.java
@@ -1,32 +1,58 @@
 package org.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.beans.ConstructorProperties;
+
+@Component
 public class Alien {
 
+    @Value("21")
     private int age;
-    private Laptop lap;
-    Alien(){
+    @Autowired
+//    @Qualifier("lap4")
+//    @Qualifier("desktop") // By default classname with first letter small is the bean name
+    private Computer comp;
+    public Alien(){
         System.out.println("Alien Object Created");
     }
+
+//    @ConstructorProperties({"age", "lap"})
+//    public Alien(int age, Laptop lap) {
+//        this.age = age;
+//        this.lap = lap;
+//        System.out.println("Parameterized Constructor called");
+//    }
 
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
-        System.out.println("Setter Called");
+//        System.out.println("Setter Called");
         this.age = age;
     }
 
-    public Laptop getLap() {
-        return lap;
+//    public Laptop getLap() {
+//        return lap;
+//    }
+//
+//    public void setLap(Laptop lap) {
+//        this.lap = lap;
+//    }
+
+    public Computer getComp() {
+        return comp;
     }
 
-    public void setLap(Laptop lap) {
-        this.lap = lap;
+    public void setComp(Computer comp) {
+        this.comp = comp;
     }
 
     public void code(){
         System.out.println("Coding...");
-        lap.compile();
+        comp.compile();
     }
 }
---
+++ SpringDemo/src/main/java/org/spring/Computer.java
@@ -0,0 +1,5 @@
+package org.spring;
+
+public interface Computer {
+    void compile();
+}
